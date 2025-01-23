5cc961156add260c42b46fc393ed42de94e92b97
wynemo <ggdabin@gmail.com>
2025-01-23 14:24:54 +0800

    format

--- ferry_web/src/views/login/index.vue
+++ ferry_web/src/views/login/index.vue
@@ -23,13 +23,20 @@
     <div class="login-weaper animated bounceInDown">
       <div class="login-left">
         <div class="login-time" v-text="currentTime" />
-        <img :src="logo" alt="" class="img">
+        <img :src="logo" alt="" class="img" />
         <p class="title" v-text="title" />
       </div>
       <div class="login-border">
         <div class="login-main">
-          <div class="login-title"> 用户登录 </div>
-          <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" autocomplete="on" label-position="left">
+          <div class="login-title">用户登录</div>
+          <el-form
+            ref="loginForm"
+            :model="loginForm"
+            :rules="loginRules"
+            class="login-form"
+            autocomplete="on"
+            label-position="left"
+          >
             <el-form-item prop="username">
               <span class="svg-container">
                 <i class="el-icon-user" />
@@ -45,7 +52,12 @@
               />
             </el-form-item>
 
-            <el-tooltip v-model="capsTooltip" content="Caps lock is On" placement="right" manual>
+            <el-tooltip
+              v-model="capsTooltip"
+              content="Caps lock is On"
+              placement="right"
+              manual
+            >
               <el-form-item prop="password">
                 <span class="svg-container">
                   <svg-icon icon-class="password" />
@@ -64,12 +76,19 @@
                   @keyup.enter.native="handleLogin"
                 />
                 <span class="show-pwd" @click="showPwd">
-                  <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+                  <svg-icon
+                    :icon-class="
+                      passwordType === 'password' ? 'eye' : 'eye-open'
+                    "
+                  />
                 </span>
               </el-form-item>
             </el-tooltip>
             <template v-if="isVerifyCodeTmp">
-              <el-form-item prop="code" style="width: 66%;float: left; margin-bottom: 13px">
+              <el-form-item
+                prop="code"
+                style="width: 66%; float: left; margin-bottom: 13px"
+              >
                 <span class="svg-container">
                   <svg-icon icon-class="validCode" />
                 </span>
@@ -82,226 +101,255 @@
                   tabindex="3"
                   maxlength="5"
                   autocomplete="off"
-                  style=" width: 75%;"
+                  style="width: 75%"
                   @keyup.enter.native="handleLogin"
                 />
               </el-form-item>
-              <div class="login-code" style="cursor:pointer; width: 30%;height: 48px;float: right;background-color: #f0f1f5;">
-                <img style="height: 48px;width: 100%;border: 1px solid rgba(0,0,0, 0.1);border-radius:5px;" :src="codeUrl" @click="getCode">
+              <div
+                class="login-code"
+                style="
+                  cursor: pointer;
+                  width: 30%;
+                  height: 48px;
+                  float: right;
+                  background-color: #f0f1f5;
+                "
+              >
+                <img
+                  style="
+                    height: 48px;
+                    width: 100%;
+                    border: 1px solid rgba(0, 0, 0, 0.1);
+                    border-radius: 5px;
+                  "
+                  :src="codeUrl"
+                  @click="getCode"
+                />
               </div>
             </template>
-            <div prop="code" style="width: 100%;float: left;margin-bottom: 13px">
+            <div
+              prop="code"
+              style="width: 100%; float: left; margin-bottom: 13px"
+            >
               <el-checkbox v-model="isLdapTmp">LDAP登陆</el-checkbox>
             </div>
-            <el-button :loading="loading" type="primary" style="width:100%;padding:12px 20px;margin-bottom:30px;" @click.native.prevent="handleLogin">
+            <el-button
+              :loading="loading"
+              type="primary"
+              style="width: 100%; padding: 12px 20px; margin-bottom: 30px"
+              @click.native.prevent="handleLogin"
+            >
               <span v-if="!loading">登 录</span>
               <span v-else>登 录 中...</span>
             </el-button>
           </el-form>
         </div>
       </div>
-
     </div>
   </div>
 </template>
 
 <script>
-
-import { getCodeImg } from '@/api/login'
-import moment from 'moment'
-import { mapGetters } from 'vuex'
+import { getCodeImg } from "@/api/login";
+import moment from "moment";
+import { mapGetters } from "vuex";
 
 export default {
-  name: 'LoginIndex',
+  name: "LoginIndex",
   data() {
     return {
       isLdapTmp: false,
-      codeUrl: '',
-      cookiePassword: '',
+      codeUrl: "",
+      cookiePassword: "",
       loginForm: {
-        username: '',
-        password: '',
+        username: "",
+        password: "",
         rememberMe: false,
-        code: '',
-        uuid: '',
-        loginType: 1
+        code: "",
+        uuid: "",
+        loginType: 1,
       },
       loginRules: {
         username: [
-          { required: true, trigger: 'blur', message: '用户名不能为空' }
+          { required: true, trigger: "blur", message: "用户名不能为空" },
         ],
         password: [
-          { required: true, trigger: 'blur', message: '密码不能为空' }
+          { required: true, trigger: "blur", message: "密码不能为空" },
         ],
-        code: [{ required: true, trigger: 'change', message: '验证码不能为空' }]
+        code: [
+          { required: true, trigger: "change", message: "验证码不能为空" },
+        ],
       },
-      passwordType: 'password',
+      passwordType: "password",
       capsTooltip: false,
       loading: false,
       redirect: undefined,
       otherQuery: {},
-      currentTime: null
-    }
+      currentTime: null,
+    };
   },
   computed: {
-    ...mapGetters(['title', 'logo', 'isLdap', 'isVerifyCode']),
-    isVerifyCodeTmp: function() {
+    ...mapGetters(["title", "logo", "isLdap", "isVerifyCode"]),
+    isVerifyCodeTmp: function () {
       if (this.isVerifyCode) {
-        this.getCode()
+        this.getCode();
       }
-      return this.isVerifyCode
-    }
+      return this.isVerifyCode;
+    },
   },
   watch: {
     $route: {
-      handler: function(route) {
-        const query = route.query
+      handler: function (route) {
+        const query = route.query;
         if (query) {
-          this.redirect = query.redirect
-          this.otherQuery = this.getOtherQuery(query)
+          this.redirect = query.redirect;
+          this.otherQuery = this.getOtherQuery(query);
         }
       },
-      immediate: true
+      immediate: true,
     },
     isLdap: {
-      handler: function(val) {
-        this.isLdapTmp = val
-      }
+      handler: function (val) {
+        this.isLdapTmp = val;
+      },
     },
     isVerifyCode: {
-      handler: function(val) {
+      handler: function (val) {
         if (val) {
-          this.getCode()
+          this.getCode();
         }
-      }
-    }
+      },
+    },
   },
   created() {
     // window.addEventListener('storage', this.afterQRScan)
-    this.getCurrentTime()
+    this.getCurrentTime();
   },
   mounted() {
-    if (this.loginForm.username === '') {
-      this.$refs.username.focus()
-    } else if (this.loginForm.password === '') {
-      this.$refs.password.focus()
+    if (this.loginForm.username === "") {
+      this.$refs.username.focus();
+    } else if (this.loginForm.password === "") {
+      this.$refs.password.focus();
     }
   },
   destroyed() {
-    clearInterval(this.timer)
+    clearInterval(this.timer);
     // window.removeEventListener('storage', this.afterQRScan)
   },
   methods: {
     getCurrentTime() {
-      this.timer = setInterval(_ => {
-        this.currentTime = moment().format('YYYY-MM-DD HH时mm分ss秒')
-      }, 1000)
+      this.timer = setInterval((_) => {
+        this.currentTime = moment().format("YYYY-MM-DD HH时mm分ss秒");
+      }, 1000);
     },
     getCode() {
-      getCodeImg().then(res => {
+      getCodeImg().then((res) => {
         if (res !== undefined) {
-          this.codeUrl = res.data
-          this.loginForm.uuid = res.id
+          this.codeUrl = res.data;
+          this.loginForm.uuid = res.id;
         }
-      })
+      });
     },
     checkCapslock({ shiftKey, key } = {}) {
       if (key && key.length === 1) {
-        this.capsTooltip = shiftKey && (key >= 'a' && key <= 'z') || !shiftKey && (key >= 'A' && key <= 'Z')
+        this.capsTooltip =
+          (shiftKey && key >= "a" && key <= "z") ||
+          (!shiftKey && key >= "A" && key <= "Z");
       }
-      if (key === 'CapsLock' && this.capsTooltip === true) {
-        this.capsTooltip = false
+      if (key === "CapsLock" && this.capsTooltip === true) {
+        this.capsTooltip = false;
       }
     },
     showPwd() {
-      if (this.passwordType === 'password') {
-        this.passwordType = ''
+      if (this.passwordType === "password") {
+        this.passwordType = "";
       } else {
-        this.passwordType = 'password'
+        this.passwordType = "password";
       }
       this.$nextTick(() => {
-        this.$refs.password.focus()
-      })
+        this.$refs.password.focus();
+      });
     },
     handleLogin() {
-      this.$refs.loginForm.validate(valid => {
+      this.$refs.loginForm.validate((valid) => {
         if (valid) {
           if (this.isLdapTmp) {
-            this.loginForm.loginType = 1
+            this.loginForm.loginType = 1;
           } else {
-            this.loginForm.loginType = 0
+            this.loginForm.loginType = 0;
           }
 
-          this.loading = true
-          this.$store.dispatch('user/login', this.loginForm)
+          this.loading = true;
+          this.$store
+            .dispatch("user/login", this.loginForm)
             .then(() => {
               // this.$router.push({ path: this.redirect || '/', query: this.otherQuery })
-              this.$router.push({ path: '/' })
-              this.loading = false
+              this.$router.push({ path: "/" });
+              this.loading = false;
             })
             .catch(() => {
-              this.loading = false
-              this.getCode()
-            })
+              this.loading = false;
+              this.getCode();
+            });
         } else {
-          return false
+          return false;
         }
-      })
+      });
     },
     getOtherQuery(query) {
       return Object.keys(query).reduce((acc, cur) => {
-        if (cur !== 'redirect') {
-          acc[cur] = query[cur]
+        if (cur !== "redirect") {
+          acc[cur] = query[cur];
         }
-        return acc
-      }, {})
-    }
-  }
-}
+        return acc;
+      }, {});
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 /* 修复input 背景不协调 和光标变色 */
 /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
-$bg:#283443;
-$light_gray:#fff;
+$bg: #283443;
+$light_gray: #fff;
 $cursor: #fff;
 
-.login-container{
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    margin: 0 auto;
-    background: url("../../assets/login.png") no-repeat;
-    background-color: #304175;
-    position: relative;
-    background-size: cover;
-    height: 100vh;
-    background-position: 50%;
+.login-container {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  background: url("../../assets/login.png") no-repeat;
+  background-color: #304175;
+  position: relative;
+  background-size: cover;
+  height: 100vh;
+  background-position: 50%;
 }
 
-#particles-js{
-    z-index: 1;
-    width: 100%;
-    height: 100%;
-    position: absolute;
+#particles-js {
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  position: absolute;
 }
 
-.login-weaper{
+.login-weaper {
   margin: 0 auto;
   width: 1000px;
-  -webkit-box-shadow: -4px 5px 10px rgba(0,0,0,.4);
-  box-shadow: -4px 5px 10px rgba(0,0,0,.4);
+  -webkit-box-shadow: -4px 5px 10px rgba(0, 0, 0, 0.4);
+  box-shadow: -4px 5px 10px rgba(0, 0, 0, 0.4);
   z-index: 1000;
 }
 
-.login-left{
+.login-left {
   border-top-left-radius: 5px;
   border-bottom-left-radius: 5px;
   -webkit-box-pack: center;
@@ -311,32 +359,32 @@ $cursor: #fff;
   -webkit-box-direction: normal;
   -ms-flex-direction: column;
   flex-direction: column;
-  background-color: rgba(64,158,255,0);
+  background-color: rgba(64, 158, 255, 0);
   color: #fff;
   float: left;
   width: 50%;
   position: relative;
-    min-height: 500px;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    align-items: center;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-  .login-time{
+  min-height: 500px;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  .login-time {
     position: absolute;
     left: 25px;
     top: 25px;
     width: 100%;
     color: #fff;
-    opacity: .9;
+    opacity: 0.9;
     font-size: 18px;
     overflow: hidden;
     font-weight: 500;
   }
 }
 
-.login-left .img{
+.login-left .img {
   width: 120px;
   height: 120px;
   border-radius: 3px;
@@ -350,36 +398,36 @@ $cursor: #fff;
   font-weight: 600;
 }
 
-.login-border{
+.login-border {
   position: relative;
-    min-height: 500px;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    align-items: center;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    border-left: none;
-    border-top-right-radius: 5px;
-    border-bottom-right-radius: 5px;
-    color: #fff;
-    background-color: hsla(0,0%,100%,.9);
-    width: 50%;
-    float: left;
+  min-height: 500px;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  border-left: none;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+  color: #fff;
+  background-color: hsla(0, 0%, 100%, 0.9);
+  width: 50%;
+  float: left;
 }
 
-.login-main{
-    margin: 0 auto;
-    width: 65%;
+.login-main {
+  margin: 0 auto;
+  width: 65%;
 }
 
-.login-title{
+.login-title {
   color: #333;
-    margin-bottom: 40px;
-    font-weight: 500;
-    font-size: 22px;
-    text-align: center;
-    letter-spacing: 4px;
+  margin-bottom: 40px;
+  font-weight: 500;
+  font-size: 22px;
+  text-align: center;
+  letter-spacing: 4px;
 }
 
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
@@ -413,18 +461,17 @@ $cursor: #fff;
   }
 
   .el-form-item {
-    border: 1px solid rgba(0,0,0, 0.1);
+    border: 1px solid rgba(0, 0, 0, 0.1);
     background: rgba(255, 255, 255, 0.8);
     border-radius: 5px;
     color: #454545;
   }
 }
-$bg:#2d3a4b;
-$dark_gray:#889aa4;
-$light_gray:#eee;
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
 
 .login-container {
-
   .tips {
     font-size: 14px;
     color: #fff;
